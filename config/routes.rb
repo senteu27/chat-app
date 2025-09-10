42589e15cd8ba7e1b5c14d81ad5407257272f23c
senteu27 <hosomiakashi1227@icloud.com>
2025-09-10 10:56:32 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
+
   resources :users,only: [:edit, :update]
+  resources :rooms,only: [:new, :create]
 
   # Defines the root path route ("/")
   # root "posts#index"
